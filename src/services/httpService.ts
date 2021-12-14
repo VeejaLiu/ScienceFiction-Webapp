77b2afe1e320d892bf6e445240e6bd568d01ab76
刘伟佳 <veejaliu@gmail.com>
2021-12-15 00:41:23 +0800

    更改环境变量名称

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -2,4 +2,4 @@ import Http from './http';
 
 export default new Http();
 
-export const basicUrl = process.env.REACT_APP_REST_BASE_URL || 'http://localhost:8080/';
+export const basicUrl = process.env.BACKEND_SERVER_URL || 'http://localhost:8080';
