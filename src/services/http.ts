f456ed001f1a108de3f08b5734fdd6fc76e186c4
刘伟佳 <veejaliu@gmail.com>
2021-12-15 00:40:43 +0800

    增加request日志

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,5 +1,8 @@
 import axios, { AxiosRequestConfig, Method } from 'axios';
 
+// const requestLog: boolean = process.env.REQUEST_LOG === 'true';
+const requestLog: boolean = true;
+
 export default class Http {
   auth: any;
 
@@ -18,13 +21,6 @@ export default class Http {
     config.url = url;
     config.method = method;
 
-    const token = this.auth.getToken();
-    if (token) {
-      config.headers = {
-        Authorization: `Bear ${token}`,
-      };
-    }
-
     if (data) {
       config.data = data;
     }
@@ -32,16 +28,19 @@ export default class Http {
   };
 
   get = (url: string, customConfig: any = {}) => {
+    requestLog && console.log('GET URL: ' + url);
     const config = this.basicConfig(url, 'GET');
     return axios({ ...config, ...customConfig }).catch(this.handleException);
   };
 
   post = (url: string, data: any, otherConfig: any = {}) => {
+    requestLog && console.log('GET POST: ' + url);
     const config = this.basicConfig(url, 'POST', data);
     return axios({ ...config, ...otherConfig }).catch(this.handleException);
   };
 
   put = (url: string, data: any, otherConfig: any = {}) => {
+    requestLog && console.log('GET PUT: ' + url);
     const config = this.basicConfig(url, 'PUT', data);
     return axios({ ...config, ...otherConfig }).catch(this.handleException);
   };
