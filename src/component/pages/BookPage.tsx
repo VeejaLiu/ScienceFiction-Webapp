de3e3bced23af2a4c3a10657fb490cff06c4a71e
VeejaLiu <veejaliu@gmail.com>
2023-03-30 01:43:02 +0800

    添加搜索状态

--- src/component/pages/BookPage.tsx
+++ src/component/pages/BookPage.tsx
@@ -11,14 +11,15 @@ function BookPage() {
     const [booksCount, setBooksCount] = useState<number>(0);
 
 
+    const [isSearch, setIsSearch] = useState<boolean>(false);
     // search book name
     const [searchBookName, setSearchName] = useState<string>("");
 
     // number of books to display per page
     const [booksPerPage, setBooksPerPage] = useState<number>(20);
-
     // current page useState, type is number
     const [currentPage, setCurrentPage] = useState<number>(1);
+
     // pageCount
     const [pageCount, setPageCount] = useState<number>(0);
     // create an array of page numbers to display in the pagination component
@@ -26,6 +27,7 @@ function BookPage() {
 
     // get books
     const getBooks = async () => {
+        setIsSearch(true);
         const getAllBookResult = await BookApi.getAllBook({
             bookName: searchBookName,
             offset: (currentPage - 1) * booksPerPage,
@@ -34,18 +36,25 @@ function BookPage() {
         console.log("getAllBookResult. books.length=" + getAllBookResult.books.length + " total=" + getAllBookResult.total);
         await setBooks(getAllBookResult.books);
         await setBooksCount(getAllBookResult.total);
-        console.log("booksCount changed! booksCount=" + booksCount)
+
+        if (getAllBookResult.total === 0) {
+            await setPageCount(0);
+            await setPageNumbers([]);
+            await setIsSearch(false);
+            return;
+        }
         const pageCount = Math.ceil(getAllBookResult.total / booksPerPage);
         await setPageCount(pageCount);
         const pageNumbers: number[] = [];
         for (let i = 1; i <= Math.ceil(getAllBookResult.total / booksPerPage); i++) {
             pageNumbers.push(i);
         }
-        setPageNumbers(pageNumbers);
+        await setPageNumbers(pageNumbers);
         console.log("pageNumbers=" + pageNumbers);
         if (currentPage > pageCount) {
             setCurrentPage(pageCount);
         }
+        setIsSearch(false);
     }
 
     // change booksPerPage or currentPage, then get books
@@ -79,7 +88,7 @@ function BookPage() {
                     />
                 </div>
 
-                <Button variant="primary" onClick={getBooks}>搜索</Button>
+                <Button variant="success" onClick={getBooks}>搜索</Button>
 
                 <Form.Select style={{width: '10rem'}} aria-label="Default select example" onChange={(event) => {
                     setBooksPerPage(parseInt(event.target.value) || 20)
@@ -92,58 +101,78 @@ function BookPage() {
                 </Form.Select>
             </div>
 
+            {(
+                isSearch && (
+                    <div style={{margin: "2rem"}}>
+                        <h1>搜索中...</h1>
+                    </div>
+                )
+            )}
 
-            {/*<h1>所有书籍</h1>*/}
-            <div style={{margin: "2rem"}}>
-                <Table striped bordered hover size="sm">
-                    <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>书名</th>
-                        <th>作者</th>
-                        <th>标签</th>
-                        <th>文件</th>
-                        <th>创建时间</th>
-                        <th>更新时间</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {
-                        books.map((book) => {
-                            return (<tr key={book.id}>
-                                <td>{book.id}</td>
-                                <td>
-                                    {/* go to book detail page */}
-                                    <Link to={`/book/${book.id}`}>
-                                        {book.bookName}
-                                    </Link>
-                                </td>
-                                <td>
-                                    <Link to={`/author/${book.bookAuthor}`}>
-                                        {book.bookAuthor}
-                                    </Link>
+            {
+                (!isSearch && books.length === 0) && (
+                    <div style={{margin: "2rem"}}>
+                        <h1>没有搜索到书籍</h1>
+                    </div>
+                )
+            }
 
-                                </td>
-                                <td>
-                                    {book.bookTags}
-                                </td>
-                                <td>
-                                    {book.bookFilePath ? (<a href="/book/#">book.bookFilePath</a>) : '暂无文件'}
-                                </td>
-                                <td>
-                                    {new Date(book.createDate).toLocaleString()}
-                                </td>
-                                <td>
-                                    {new Date(book.updateDate).toLocaleString()}
-                                </td>
-                            </tr>);
-                        })}
-                    </tbody>
-                </Table>
-            </div>
-            {/* 所有书籍 End */}
+            {
+                (!isSearch && books.length > 0) && (
+                    <div style={{margin: "2rem"}}>
+                        <h1>搜索到 {booksCount} 本书籍</h1>
+                        <Table striped bordered hover size="sm">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>书名</th>
+                                <th>作者</th>
+                                <th>标签</th>
+                                <th>文件</th>
+                                <th>创建时间</th>
+                                <th>更新时间</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {
+                                books.map((book) => {
+                                    return (<tr key={book.id}>
+                                        <td>{book.id}</td>
+                                        <td>
+                                            {/* go to book detail page */}
+                                            <Link to={`/book/${book.id}`}>
+                                                {book.bookName}
+                                            </Link>
+                                        </td>
+                                        <td>
+                                            <Link to={`/author/${book.bookAuthor}`}>
+                                                {book.bookAuthor}
+                                            </Link>
 
-            {(pageNumbers.length > 0) && (
+                                        </td>
+                                        <td>
+                                            {book.bookTags}
+                                        </td>
+                                        <td>
+                                            {book.bookFilePath ? (<a href="/book/#">book.bookFilePath</a>) : '暂无文件'}
+                                        </td>
+                                        <td>
+                                            {new Date(book.createDate).toLocaleString()}
+                                        </td>
+                                        <td>
+                                            {new Date(book.updateDate).toLocaleString()}
+                                        </td>
+                                    </tr>);
+                                })}
+                            </tbody>
+                        </Table>
+
+
+                    </div>
+                )
+            }
+
+            {(pageNumbers.length > 0 && !isSearch) && (
                 <>
                     {/* 分页组件 居中显示 */}
                     <div style={{margin: "auto 2rem"}}>
