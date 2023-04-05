4c0128a6ad7b26bb32eae116489c9ad429717671
VeejaLiu <veejaliu@gmail.com>
2023-04-05 16:54:45 +0800

    修改网站图标

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -9,30 +9,30 @@ function NavHeader() {
         <div>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="/">Science Fiction Webapp</Navbar.Brand>
+                    <Navbar.Brand href="/">科 幻 小 说 网<br/>Science Fiction Website</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="/book">书籍</Nav.Link>
                             <Nav.Link href="/author">作者</Nav.Link>
                             <Nav.Link href="/category">分类</Nav.Link>
-                            <Nav.Link href="/file">文件</Nav.Link>
+                            {/*<Nav.Link href="/file">文件</Nav.Link>*/}
                             <NavDropdown title="用户" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="/user/profile">
                                     个人中心
                                 </NavDropdown.Item>
-                                <NavDropdown.Item href="/user/myUpload">
-                                    我的资源
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/user/myFavorite">
-                                    我的收藏
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/user/message">
-                                    消息中心
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/user/reward">
-                                    签到抽奖
-                                </NavDropdown.Item>
+                                {/*<NavDropdown.Item href="/user/myUpload">*/}
+                                {/*    我的资源*/}
+                                {/*</NavDropdown.Item>*/}
+                                {/*<NavDropdown.Item href="/user/myFavorite">*/}
+                                {/*    我的收藏*/}
+                                {/*</NavDropdown.Item>*/}
+                                {/*<NavDropdown.Item href="/user/message">*/}
+                                {/*    消息中心*/}
+                                {/*</NavDropdown.Item>*/}
+                                {/*<NavDropdown.Item href="/user/reward">*/}
+                                {/*    签到抽奖*/}
+                                {/*</NavDropdown.Item>*/}
                                 <NavDropdown.Divider/>
                                 <NavDropdown.Item href="/user/logout">
                                     登出
